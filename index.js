7da3c6fa18422148f5830fd7aaa22c85c8d159ca
Logesh Kumar R <phoenix.logu@gmail.com>
2017-01-07 23:21:51 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+var express = require('express')
+var app = express()
+
+app.get('/', function (req, res) {
+  res.send('Hello Digital Ocean!')
+})
+
+app.listen(8000, function () {
+  console.log('Magic is happening on port 8000!')
+})
